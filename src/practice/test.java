2f20f7c54dbf478641c056ac5c7aa1210abb0fa3
shanquanda <2530252151@qq.com>
2024-01-06 16:53:58 +0800

    test

--- src/practice/test.java
+++ src/practice/test.java
@@ -152,7 +152,9 @@ public class test {
 //        public String toString() {
 //            return s1 + s2;
 //        }
-
+    public static void main(String[] args) {
+        System.out.println("sss");
+    }
 
 
 }
